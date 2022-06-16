785ed70708411834705ed6494cc30495b1cbc590
Duan Chaofan <dchaof888@163.com>
2022-06-16 23:34:57 +0800

    ‘修改属性值按钮事件
    git commit -m ‘修改属性值按钮事件

--- src/views/product/attr/List.vue
+++ src/views/product/attr/List.vue
@@ -5,7 +5,7 @@
     </el-card>
     <el-card style="margin-top:20px">
       <div v-show="isShowList">
-        <el-button type="primary" icon="el-icon-plus">添加属性值</el-button>
+        <el-button type="primary" icon="el-icon-plus" :disabled="!category3Id" @click="showAddDiv">添加属性值</el-button>
         <el-table
         border
         :data="attrList"
@@ -33,8 +33,8 @@
           prop="address"
           label="操作"
           width="150">
-          <template>
-              <HintButton type="warning" icon="el-icon-edit" size="mini" title="修改"></HintButton>
+          <template slot-scope="{row,$index}">
+              <HintButton type="warning" icon="el-icon-edit" size="mini" title="修改" @click="updateAttrValue(row)"></HintButton>
               <HintButton type="danger" icon="el-icon-delete" size="mini" title="删除"></HintButton>
           </template>
         </el-table-column>
@@ -43,14 +43,16 @@
       <div v-show="!isShowList">
         <el-form :model="attrForm" :inline="true" >
           <el-form-item
-            label="属性名">
+            label="属性名"
+            >
             <el-input type="age" v-model="attrForm.attrName" placeholder="请输入属性名"></el-input>
           </el-form-item>
         </el-form>
-        <el-button type="primary" icon="el-icon-plus" >添加属性值</el-button>
-        <el-button >取消</el-button>
+        <el-button type="primary" icon="el-icon-plus" :disabled="!attrForm.attrName" @click="addAttrValue">添加属性值</el-button>
+        <el-button @click="isShowList = true">取消</el-button>
 
         <el-table
+          :data="attrForm.attrValueList"
           border
           style="width: 100%;margin: 20px 0">
           <el-table-column
@@ -62,14 +64,20 @@
           <el-table-column
             label="属性值名称"
             width="width">
+            <template slot-scope="{row,$index}">
+              <el-input v-model="row.valueName" placeholder=""></el-input>
+            </template>
           </el-table-column>
           <el-table-column
             label="操作"
             width="width">
+            <template slot-scope="{row,$index}">
+              <HintButton type="danger" icon="el-icon-delete" size="mini" title="删除"></HintButton>
+            </template>
           </el-table-column>
         </el-table>
         <el-button type="primary" >保存</el-button>
-        <el-button >取消</el-button>
+        <el-button @click="isShowList = true">取消</el-button>
 
       </div>
     </el-card>
@@ -77,6 +85,7 @@
 </template>
 
 <script>
+import cloneDeep from 'lodash/cloneDeep'
 export default {
   name: 'attr',
   data(){
@@ -85,10 +94,20 @@ export default {
       category2Id:'',
       category3Id:'',
       attrList:[],
-      isShowList:false,//控制第二个card显示添加还是列表
+      isShowList:true,//控制第二个card显示添加还是列表
       attrForm:{
-        attrName:''
+        attrName:'',
+        attrValueList:[
+          /* {
+            attrId:0,
+            id:0,
+            valueName:''
+          } */
+        ],
+        categoryId:0,
+        categoryLevel:3
       },
+
     }
   },
   methods:{
@@ -109,6 +128,24 @@ export default {
       if(result.code === 200){
         this.attrList = result.data
       }
+    },
+    showAddDiv(){
+      this.isShowList = false;
+      //点击完添加  清空添加列表中的信息
+      this.attrForm.attrName = '';
+      this.attrForm.attrValueList = []
+      this.attrForm.categoryId = this.category3Id
+    },
+    addAttrValue(){
+      this.attrForm.attrValueList.push({
+          attrId:this.attrForm.id,
+          valueName:''
+      })
+    },
+    updateAttrValue(row){
+      this.isShowList = false;
+      //一定要深拷贝
+      this.attrForm = cloneDeep(row)
     }
   }
 }
