7f747c48329f495d46176da01dbc1cb0268c0450
Duan Chaofan <dchaof888@163.com>
2022-06-17 19:26:05 +0800

    编辑模式和查看模式的切换自动获取焦点

--- src/views/product/attr/List.vue
+++ src/views/product/attr/List.vue
@@ -65,7 +65,9 @@
             label="属性值名称"
             width="width">
             <template slot-scope="{row,$index}">
-              <el-input v-model="row.valueName" placeholder=""></el-input>
+              <!-- 一定要使用 native  使自定义事件转化为原生的事件  keyup再elinput组件中没有emit -->
+              <el-input v-model="row.valueName" placeholder="请输入添加的属性值" v-if="row.isEdit === true" @blur="toLook(row)" @keyup.enter.native="toLook(row)" size="mini" :ref="$index"></el-input>
+              <span v-else @click="toEdit(row,$index)" style="display:block;width:100%;height:100%;">{{row.valueName}}</span>
             </template>
           </el-table-column>
           <el-table-column
@@ -132,20 +134,69 @@ export default {
     showAddDiv(){
       this.isShowList = false;
       //点击完添加  清空添加列表中的信息
-      this.attrForm.attrName = '';
-      this.attrForm.attrValueList = []
-      this.attrForm.categoryId = this.category3Id
+      this.attrForm = {
+        attrName : '',
+        attrValueList : [],
+        categoryId : this.category3Id
+      }
+
     },
     addAttrValue(){
       this.attrForm.attrValueList.push({
           attrId:this.attrForm.id,
-          valueName:''
+          valueName:'',
+          isEdit:true//添加属性值的时候  用来标识是否是编辑模式
       })
+      this.$nextTick(()=>{
+        this.$refs[this.attrForm.attrValueList.length-1].focus();
+      })
+
     },
     updateAttrValue(row){
       this.isShowList = false;
       //一定要深拷贝
       this.attrForm = cloneDeep(row)
+      this.attrForm.attrValueList.forEach(item => {
+        //设置响应式必须用 vm.$set 或 VM.set 设置数据  否则不是响应式  
+        //或者直接操作对应的对象
+        //再生命周期中 只有再beforeCreate 到 created 初始化的数据才是响应式的
+        this.$set(item,'isEdit',false)
+      })
+    },
+    toLook(row){
+      let valueName = row.valueName
+      if(!valueName.trim()){
+        this.$message.info('属性值不可以为空')
+        return 
+      }
+      let isRepeat = this.attrForm.attrValueList.some(item =>{
+        if(item !== row){
+          this.$message.info('这个属性值已经存在了')
+          return item.valueName === valueName.trim()
+        }
+      })
+      if(isRepeat){
+        return 
+      }
+      row.isEdit = false
+    },
+    //点击之后san变为input
+    toEdit(row,index){
+      row.isEdit = true
+      //自动获取焦点的功能，根据index就知道哪个input要获取焦点 然后这个index值正好对应的使ref的值
+      //为何this.$refs[index] 为undefined
+      //因为我们获取这个焦点太快了
+      //上面我们把row.isEdit === true之后，由于dom是用vif进行创建的  紧接着就去获取节点
+      //此时input还没有创建好 所以获取到的是undefined
+      //$nextTick 代表的是 页面最后一次更新完之后执行的回调
+      this.$nextTick(()=>{
+        //等待页面更新完成之后再去获取input节点
+        this.$refs[index].focus();
+
+        //$nextTick   和 updated的区别
+        //$nextTick  是最近一次页面更新完执行的回调  只执行一次
+        //updated   只要页面发生更新就会执行一次
+      })
     }
   }
 }
